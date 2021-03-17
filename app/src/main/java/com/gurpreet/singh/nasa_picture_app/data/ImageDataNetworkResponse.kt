1915577f4e06826512e8a0795521c9aa025bddd2
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-17 23:18:02 +0530

    update network response call

--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/data/ImageDataNetworkResponse.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/data/ImageDataNetworkResponse.kt
@@ -6,22 +6,5 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ImageDataNetworkResponse(
-
-        @Json(name = "copyright")
-        val copyright: String,
-        @Json(name = "date")
-        val date: String,
-        @Json(name = "explanation")
-        val explanation: String,
-        @Json(name = "hdurl")
-        val hdurl: String,
-        @Json(name = "media_type")
-        val media_type: String,
-        @Json(name = "service_version")
-        val service_version: String,
-        @Json(name = "title")
-        val title: String,
-        @Json(name = "url")
-        val url: String
-
+        val listImages: List<ImageData>
 ) : Parcelable
