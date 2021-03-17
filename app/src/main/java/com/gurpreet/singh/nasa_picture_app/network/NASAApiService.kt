687af0e0a3ed9101a63f59f05978fd9d51b2873d
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-17 14:10:53 +0530

    changed class to interface for NASAApiService.kt, added api interface and initialized the retrofitService

--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/network/NASAApiService.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/network/NASAApiService.kt
@@ -1,13 +1,17 @@
 package com.gurpreet.singh.nasa_picture_app.network
 
+import com.gurpreet.singh.nasa_picture_app.data.ImageDataNetworkResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
 
-private const val BASE_URL = "https://mrandrioddev.github.io/mockApis.github.io/"
+private const val BASE_URL = "https://mrandrioddev.github.io/"
 
 
 private val client = OkHttpClient.Builder().apply {
@@ -25,5 +29,13 @@ private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .build()
 
-class NASAApiService {
+interface NASAApiService {
+    @GET("mockApis.github.io")
+    fun getImagesData(@QueryMap params: Map<String, String>): Deferred<ImageDataNetworkResponse>
+}
+
+object IconsApi {
+    val retrofitService: NASAApiService by lazy {
+        retrofit.create(NASAApiService::class.java)
+    }
 }
