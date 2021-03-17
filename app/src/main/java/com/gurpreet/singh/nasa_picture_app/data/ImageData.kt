8c6b4d605be1eb5481c14948ef53b598684179ee
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-17 11:58:23 +0530

    added data file for  server response

---
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/data/ImageData.kt
@@ -0,0 +1,27 @@
+package com.gurpreet.singh.nasa_picture_app.data
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ImageData(
+
+        @Json(name = "copyright")
+        val copyright: String,
+        @Json(name = "date")
+        val date: String,
+        @Json(name = "explanation")
+        val explanation: String,
+        @Json(name = "hdurl")
+        val hdurl: String,
+        @Json(name = "media_type")
+        val media_type: String,
+        @Json(name = "service_version")
+        val service_version: String,
+        @Json(name = "title")
+        val title: String,
+        @Json(name = "url")
+        val url: String
+
+) : Parcelable
