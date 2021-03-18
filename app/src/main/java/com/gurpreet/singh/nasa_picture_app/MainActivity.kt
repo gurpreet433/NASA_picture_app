d6a3e7bd6ed39c820995ecdc83337fb5830004c4
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-18 18:30:47 +0530

    added back key in toolbar

--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/MainActivity.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/MainActivity.kt
@@ -2,10 +2,29 @@ package com.gurpreet.singh.nasa_picture_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController!!)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        return navController.navigateUp()
     }
 }
