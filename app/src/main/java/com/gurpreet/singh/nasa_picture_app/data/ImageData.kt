e4c7b417cb9de661ba49c0cba652993ecef6c12a
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-18 09:25:01 +0530

    allow null value as some values are missing in the data

--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/data/ImageData.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/data/ImageData.kt
@@ -8,20 +8,20 @@ import kotlinx.android.parcel.Parcelize
 data class ImageData(
 
         @Json(name = "copyright")
-        val copyright: String,
+        val copyright: String?,
         @Json(name = "date")
-        val date: String,
+        val date: String?,
         @Json(name = "explanation")
-        val explanation: String,
+        val explanation: String?,
         @Json(name = "hdurl")
-        val hdurl: String,
+        val hdurl: String?,
         @Json(name = "media_type")
-        val media_type: String,
+        val media_type: String?,
         @Json(name = "service_version")
-        val service_version: String,
+        val service_version: String?,
         @Json(name = "title")
-        val title: String,
+        val title: String?,
         @Json(name = "url")
-        val url: String
+        val url: String?
 
 ) : Parcelable
