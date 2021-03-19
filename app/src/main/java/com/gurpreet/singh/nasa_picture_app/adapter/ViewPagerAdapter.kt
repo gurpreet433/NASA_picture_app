1fe1d91477f5d57af907ce725cf4e78a0c688380
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-19 17:01:30 +0530

    Added viewpager2 adapter class

---
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.gurpreet.singh.nasa_picture_app.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gurpreet.singh.nasa_picture_app.R
+import com.gurpreet.singh.nasa_picture_app.data.ImageData
+import kotlinx.android.synthetic.main.layout_image_details_item.view.*
+
+class ViewPagerAdapter(val imageDataList: ArrayList<ImageData>) :
+    RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+
+    inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_image_details_item, parent, false)
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        var currentImageData = imageDataList[position]
+        holder.itemView.title.text = currentImageData.title
+        holder.itemView.hd_image.loadImageFromUrl(currentImageData.hdurl)
+
+    }
+
+    override fun getItemCount(): Int {
+        return imageDataList.size
+    }
+}
