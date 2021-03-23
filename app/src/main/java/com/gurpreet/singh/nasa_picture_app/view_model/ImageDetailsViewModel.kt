a8a482517c6ff8967be762d6a2ccb3e0a74f8ea9
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-24 00:36:19 +0530

    removed unused imports

--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/view_model/ImageDetailsViewModel.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/view_model/ImageDetailsViewModel.kt
@@ -1,7 +1,6 @@
 package com.gurpreet.singh.nasa_picture_app.view_model
 
 import androidx.lifecycle.ViewModel
-import com.gurpreet.singh.nasa_picture_app.adapter.ViewPagerAdapter
 import com.gurpreet.singh.nasa_picture_app.data.ImageData
 
 class ImageDetailsViewModel(imageData: ImageData, imageDataList: ArrayList<ImageData>): ViewModel() {
--- app/src/main/java/com/gurpreet/singh/nasa_picture_app/views/ImageDetailsFragment.kt
+++ app/src/main/java/com/gurpreet/singh/nasa_picture_app/views/ImageDetailsFragment.kt
@@ -1,12 +1,10 @@
 package com.gurpreet.singh.nasa_picture_app.views
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.collection.arraySetOf
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
