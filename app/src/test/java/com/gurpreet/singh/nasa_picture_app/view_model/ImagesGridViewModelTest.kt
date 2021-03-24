037f01d39d6f46d9751d00a70c91ef49db25487a
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-24 08:48:02 +0530

    added first test

---
+++ app/src/test/java/com/gurpreet/singh/nasa_picture_app/view_model/ImagesGridViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.gurpreet.singh.nasa_picture_app.view_model
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ImagesGridViewModelTest : TestCase() {
+
+    @Test
+    fun getImageData_ImagesGridViewModel() {
+
+        val getImageViewModel = ImagesGridViewModel()
+
+    }
+}
